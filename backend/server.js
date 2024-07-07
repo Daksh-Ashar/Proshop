e0a1261da77d943fa5e109ea2fca9a98ad7e3d5b
Daksh-Ashar <dakshashardpa29@gmail.com>
2024-07-07 17:06:05 -0500

    bundling script added

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,17 @@ const __dirname = path.resolve();
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')));
 app.use(errorHandler);
 
-app.listen(port, (req,res)=>{
-    console.log(`Server is running on Port: ${port}`);
-});
+if(process.env.NODE_ENV === "production")
+    {
+        //set static folder
+        app.use(express.static(path.join(__dirname,'/frontend/build')));
+
+        //any route that is not api will be redirected to index.html
+        app.get("*",(req,res)=>{
+            res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
+        })
+    }else{
+        app.listen(port, (req,res)=>{
+            console.log(`Server is running on Port: ${port}`);
+        });
+    }
